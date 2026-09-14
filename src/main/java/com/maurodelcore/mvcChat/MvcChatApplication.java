49f007a40b9ed15c5c7b9a5ece2aa8bfc951b50a
Mauro Castrese Del Core <61396777+Maurodc90@users.noreply.github.com>
2026-09-14 16:11:48 +0300

    starter project

---
+++ src/main/java/com/maurodelcore/mvcChat/MvcChatApplication.java
@@ -0,0 +1,13 @@
+package com.maurodelcore.mvcChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcChatApplication.class, args);
+	}
+
+}
